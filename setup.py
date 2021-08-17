e6a1b4914b0e177770af3c79bed5e91a6ede35e5
hgpark <hgpark@fuels.ai>
2021-08-17 19:21:06 +0900

    description change

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name="pytest-bg-process",
-    version="0.0.1",
+    version="0.0.2",
     author="IndlgO",
     author_email="hanguyl91@gmail.com",
     maintainer="IndlgO",
